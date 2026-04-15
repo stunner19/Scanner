4007efa4d9fa00781e1c3d7352fa95edb36f39bb
Karanpal Singh <singhkaranpal81@gmail.com>
2026-04-16 00:17:14 +0530

    Revert "Improve dropdown latency"
    This reverts commit 75b5bdbe8e00047026ff9717b3bd6e7cb190d2c4.
    
    Revert the changes

--- backend/universe/__init__.py
+++ backend/universe/__init__.py
@@ -1,6 +1,1 @@
-from .india_stocks import (
-    get_universe,
-    get_universe_catalog,
-    get_universe_names,
-    warm_universe_cache,
-)
+from .india_stocks import get_universe, get_universe_names
